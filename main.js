b41547f41196faa418e010f2ddd5b12e7fedb4f0
Sk-Shubhasheesh <shubhasheeshkundu02@gmail.com>
2024-09-01 06:56:42 +0530

    LEarn BAsic of GSAP

---
+++ main.js
@@ -0,0 +1,26 @@
+// gsap.to("#box", {
+//     // y:500,
+//     // duration: 3,
+//     // delay:3
+
+//     x:1000,
+//     rotate:360,
+//     backgroundColor: "blue",
+//     duration: 2,
+//     delay:1,   
+// })
+
+gsap.from("#box", {
+    // y:500,
+    // duration: 3,
+    // delay:3
+
+    x:1000,
+    rotate:360,
+    backgroundColor: "blue",
+    duration: 2,
+    delay:1,   
+})
+
+
+
